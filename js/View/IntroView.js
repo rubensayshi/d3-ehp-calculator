8a0b049981b8ff90126bc569516d6cb5ae17c5b3
Ruben de Vries <ruben@rubensayshi.com>
2012-06-02 19:55:26 +0200

    added delete button

--- js/View/IntroView.js
+++ js/View/IntroView.js
@@ -21,19 +21,37 @@ var IntroView = Backbone.View.extend({
         $charlist = $('.character-list', this.el);
         
         CharacterList.each(function(character) {
+            var $li = $('<li />')              
+                        .css({'cursor':     'pointer',
+                            'margin-top': '5px'
+                        })
+                        .appendTo($charlist);
+
             $('<button />')
                 .addClass('btn btn-inverse')
                 .append($('<i />').addClass('icon-user icon-white'))
                 .append($('<span />').css('padding-left', '15px').html(character.get('description')))
                 .css({'cursor':     'pointer',
                       'min-width':  '30%',
-                      'margin-top': '5px',
                       'text-align': 'left'
                 })
                 .click(_.bind(function() {
                     this.mainView.changeView(function(contentEl, mainView) { return new SimulationView({'el': contentEl, 'mainView': mainView, 'model': character}); });
                 }, this))
-                .appendTo($('<li />').appendTo($charlist));
+                .appendTo($li);
+            
+            $('<button />')
+                .addClass('btn btn-danger')
+                .append($('<i />').addClass('icon-remove icon-white'))
+                .css({'cursor':     'pointer',
+                      'text-align': 'left',
+                      'margin-left':'10px'
+                })
+                .click(_.bind(function() {
+                    character.destroy();
+                    $li.remove();
+                }, this))
+                .appendTo($li);
         }, this);
     }
 });
