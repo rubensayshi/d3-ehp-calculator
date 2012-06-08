613ca1fe2f3903455ed4a79239dd23efe7ac06a3
Ruben de Vries <ruben@rubensayshi.com>
2012-06-08 13:55:53 +0200

    building an index.html with versioned URLs to cdn subdomain

---
+++ build.php
@@ -0,0 +1,14 @@
+<?php
+
+$dir = dirname(__FILE__);
+
+if (!file_exists("{$dir}/index.html")) {
+    exit(1);
+}
+
+$index = file_get_contents("{$dir}/index.html");
+$index = str_replace('src="js/vendor/', 'src="http://d3ehp-cdn.rubensayshi.com/r' . time() . '/js/vendor/', $index);
+
+file_put_contents("{$dir}/versioned_index.html", $index);
+
+?>
