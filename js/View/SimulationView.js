062d44abd352d7107dcce6f25706b37b748450ee
Ruben de Vries <ruben@rubensayshi.com>
2012-06-11 09:39:39 +0200

    removed console.log

--- js/View/SimulationView.js
+++ js/View/SimulationView.js
@@ -319,8 +319,6 @@ var SimulationView = Backbone.View.extend({
             $(selector, this.el).val(ehp_change);
             $(selector + '.percentage', this.el).val(ehp_change_p);
         }, this);
-        
-        console.log(compareModel.get('ehp'));
     },
     
     renderClass: function() {
